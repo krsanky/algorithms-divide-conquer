5ad7fa71d2874355ec93931ad74719b2d3e7b0e0
paul wisehart <paul@oldcode.org>
2019-03-11 15:10:51 -0400

    most basic error handling

--- a1.scm
+++ a1.scm
@@ -1,4 +1,21 @@
-(import chicken.format)
+(import chicken.format chicken.condition)
+
+(define fn "input_1")
 
 (printf "assignmnet #1\n")
+(printf "reading in file ...\n")
+
+#|
+(exn i/o file) 
+
+(condition-case
+	[fp (open-input-file "input_1")])
+	(close-input-port fp))
+|#
+	
+(condition-case 
+	(let* (
+		[fp (open-input-file fn)])
+		(print "do something with fp..."))
+	[(exn file) (print "file error")])
 
