1905e23dd9a31e1c5de49cb0567796c769305602
paul wisehart <paul@oldcode.org>
2019-03-12 09:32:24 -0400

    file io stuff

--- a1.scm
+++ a1.scm
@@ -12,10 +12,16 @@
 	[fp (open-input-file "input_1")])
 	(close-input-port fp))
 |#
-	
+
+(define (get-2-numbers fp)
+	(print "get 2 number ..."))
+
 (condition-case 
 	(let* (
-		[fp (open-input-file fn)])
-		(print "do something with fp..."))
+		[fp (open-input-file fn)]
+		)
+		(print "do something with fp...")
+		(print (read fp)))
+
 	[(exn file) (print "file error")])
 
