501a6e6233bb2cdfc8161af0d6b4e9ccfc56b64e
paul wisehart <paul@oldcode.org>
2019-03-15 13:14:26 -0400

    commit before work from x240

--- a1.scm
+++ a1.scm
@@ -5,20 +5,34 @@
 
 (define fn "input_1")
 
-(printf "assignmnet #1\n")
-
 (define (get-2-numbers fp)
 	(list (read fp) (read fp)))
+
+(define (print-pass s)
+	(print s)
+	s)
 
 (define (big-mult l)
 	(printf "big mult:\n~a\n~a\n" (car l) (cadr l)))
 
-(condition-case 
-	(let* ([fp (open-input-file fn)])
-		(-> fp 
-			get-2-numbers 
-			big-mult)
-		(close-input-port fp)
-	)
-	[(exn file) (print "file error")])
+(define s1 (lambda (s) s))
 
+(define (main1)
+	(printf "assignmnet #1\n")
+	(condition-case 
+		(let* ([fp (open-input-file fn)])
+			(-> fp 
+				get-2-numbers 
+				print-pass
+				s1
+;;( karatsuba-mult 
+				big-mult)
+			(close-input-port fp)
+		)
+		[(exn file) (print "file error")])
+)
+
+(define (karatsuba-mult n1 n2)
+	((lambda (a1 a2) (printf "lambda:\n1:~a\n2:~a\n" a1 a2)) n1 n2))
+
+(main1)
