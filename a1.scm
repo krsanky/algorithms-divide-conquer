ec56c7130e3b2416a9bfa465ae9bf8d4aaa0e5e1
paul wisehart <paul@oldcode.org>
2019-03-16 09:36:15 -0400

    slow...

--- a1.scm
+++ a1.scm
@@ -8,14 +8,8 @@
 (define (get-2-numbers fp)
 	(list (read fp) (read fp)))
 
-(define (print-pass s)
-	(print s)
-	s)
-
 (define (big-mult l)
 	(printf "big mult:\n~a\n~a\n" (car l) (cadr l)))
-
-(define s1 (lambda (s) s))
 
 (define (main1)
 	(printf "assignmnet #1\n")
@@ -23,16 +17,25 @@
 		(let* ([fp (open-input-file fn)])
 			(-> fp 
 				get-2-numbers 
-				print-pass
-				s1
-;;				(as-> l (karatsuba-mult (car l) (cdr l)))
 				big-mult)
 			(close-input-port fp)
 		)
 		[(exn file) (print "file error")])
 )
 
+(define (main2)
+	(printf "main2...\n")
+	(let (
+		[x 1234]
+		[y 5678])
+		(printf "x:~a y:~a\n" x y)
+		(ktest1 x y)))
+
 (define (karatsuba-mult n1 n2)
 	((lambda (a1 a2) (printf "lambda:\n1:~a\n2:~a\n" a1 a2)) n1 n2))
 
-(main1)
+(define (ktest1 n1 n2)
+	(print (* n1 n2)))
+
+(main2)
+
--- cl1.scm
+++ cl1.scm
@@ -8,12 +8,15 @@
 	(printf "2args a1:~a a2:~a\n" a1 a2)
 	(list a1 a2))
 
+(define fl (lambda (x) (printf "lambda x:~a\n" x) x))
+
 (define (main)
 	(begin
 		(printf "do stuff\n")
 ;;		(f2args "asd" "qwe")
 		(-> "asd"
 			f1
+			fl
 			(as-> x (f2args x x))
 			f1)))
 
